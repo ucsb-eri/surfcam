214acdae12baf9e6c319cfb0307ec498269e76e9
slapplebags <richardnixonkingofthemoleppl@gmail.com>
2022-10-24 15:25:24 -0700

    initial commit

---
+++ noaawave.py
@@ -0,0 +1,20 @@
+import requests
+import os
+
+def print_hi(name):
+    open("/surf/wavedata.txt").close() 
+    print('Beginning file download with requests')
+    url = 'https://www.ndbc.noaa.gov/data/realtime2/46053.spec'
+    r = requests.get(url)
+    with open('/surf/wavedata.txt', 'wb') as f:
+        f.write(r.content)
+    file = open('/surf/wavedata.txt')
+    content = file.readlines()
+    data = content[0]+content[1]+content[2]
+    with open('/surf/wavedata.txt', 'w') as f:
+        f.write(data)
+    print(data)
+    f.close
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
